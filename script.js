45708f1644010517605286ca7d4c91163bfaa582
Thiago Elias <contatothiagoelias22@gmail.com>
2023-07-23 00:50:44 -0300

    Começo

---
+++ script.js
@@ -0,0 +1,19 @@
+
+//Feito para fazer com que ao clicar no pointer mude o slide e ative o pointer correto.
+const pointers = document.querySelectorAll(".pointer");
+const slides = document.querySelector(".slides");
+const slideWidth = 100; // Largura de cada slide em vw
+let currentIndex = 0; // Índice do slide atual
+
+pointers.forEach(function(pointer, index) {
+  pointer.addEventListener('click', function() {
+    pointers.forEach(function(otherPointer) {
+      otherPointer.classList.remove('active');
+    });
+    pointer.classList.add('active');
+    currentIndex = index;
+    slides.style.marginLeft = `-${currentIndex * slideWidth}vw`;
+  });
+});
+
+// Quero adicionar depois para que a cada segundo mude o display
